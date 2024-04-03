ac1abbe2e6ff1f6e4bc02025ad6b37a302efed21
Doctor-Johnson <165823128+Doctor-Johnson@users.noreply.github.com>
2024-04-03 11:18:27 -0400

    Update enemies.h Sean and Kevin

--- enemies.h
+++ enemies.h
@@ -1,8 +1,23 @@
 #ifndef ENEMIES_H
 #define ENEMIES_H
 
+#include <string>
+#include "room.h"
+
 class Enemies {
-    //attack
+friend class Room;
+    public:
+        Enemies(std::string name, int HP, int atk);
+        ~Enemies();
+        std::string getEnemyName();
+        int getHP();
+        int getMaxHP();
+        int getAttack();
+    private:
+        std::string enemyName;
+        int HP;
+        int maxHP;
+        int atk;
 };
 
 #endif
